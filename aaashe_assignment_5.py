d90dee759f2ee09058c2ebdb3132e505b2f22b65
Abraheem Ashe <aaashe@aggies.ncat.edu>
2025-10-03 01:15:13 -0400

    Challenge 2: Prime checker - for loop for known range

--- aaashe_assignment_5.py
+++ aaashe_assignment_5.py
@@ -1,3 +1,5 @@
+#Chat gpt was used on lines 6 and 13 to help print in the correct format.
+
 current_number = int(input("Enter starting number: "))
 step_count = 0
 
@@ -11,3 +13,15 @@ while current_number != 1:
     print(current_number, end=" ")
     step_count += 1
 print(f'\nSteps: {step_count}')
+
+prime_num = int(input("Enter number: "))
+
+while prime_num > 1:
+    print(f"Testing divisors from 2 to {prime_num - 1}...")
+    for prime_range in range(2, (prime_num)):
+        if (prime_num % prime_range) == 0:
+            print(f"{prime_num} is not a prime number.")
+            break
+    else:
+        print(f"{prime_num} is a prime number.")
+        break
